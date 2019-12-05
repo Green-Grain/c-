b0dc3824fcb229df97d8474205e9cd42c5ce50ba
Green-Grain <zwei.blattes@gmail.com>
2019-12-05 16:29:08 +0900

    処理改善＋名称訂正

--- variable_digit_permutation/test.cpp
+++ variable_digit_permutation/test.cpp
@@ -6,27 +6,28 @@ using namespace std;
 
 const char words[] = "abcdefghij";
 const int wordNum  = (sizeof(words) / sizeof(words[0])) - 1;
-typedef list<string> string_list;
 
 /*!
  * @brief 文字組み合わせ
- * @param digitNth      [in]  何桁目
- * @param digitMax      [in]  最大桁数
- * @param p_result      [in/out]  組み合わせ後の文字列
- * @param p_resultList  [out] 組み合わせ後の文字列リスト
+ * @param digitNth    [in]  何桁目
+ * @param digitMax    [in]  最大桁数
+ * @param p_result    [in/out]  組み合わせ後の文字列
+ * @param outputFile  [out] 出力先ファイル
 */
-void  combinateWord(int digitNth, int digitMax, char* p_result, string_list* p_resultList) {
+void  combinateWord(int digitNth, int digitMax, char* p_result, ofstream& outputFile) {
   if (nullptr == p_result) return;
-  if (nullptr == p_resultList) return;
 
   for (int wordIndex = 0; wordIndex < wordNum; wordIndex++) {
     int setIndex = digitMax - digitNth;
     p_result[setIndex] = words[wordIndex];
     if (1 < digitNth) {
-      combinateWord(digitNth - 1, digitMax, p_result, p_resultList);
+      combinateWord(digitNth - 1, digitMax, p_result, outputFile);
     }
     else {
-      p_resultList->push_back(string(p_result));
+      string result(p_result);
+      result += "\n";
+      printf("%s", result.c_str());
+      outputFile << result.c_str();
     }
   }
 }
@@ -39,15 +40,8 @@ int main() {
   char* p_result = new char[digit];
   if (nullptr == p_result)  return -1;
   memset(p_result, 0, sizeof(char) * digit);
-  string_list resultList;
-  combinateWord(digit, digit, p_result, &resultList);
   ofstream outputFile("result.txt");
-  string_list::iterator itr = resultList.begin();
-  for( ; itr != resultList.end(); ++itr) {
-    *itr = *itr + "\n";
-    printf("%s", itr->c_str());
-    outputFile << itr->c_str();
-  }
+  combinateWord(digit, digit, p_result, outputFile);
   outputFile.close();
   if (nullptr != p_result) {
     delete[] p_result;
