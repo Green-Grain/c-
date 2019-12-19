1fc7a5d8b082fd0adbb9de3963361b5b0912e2d6
Green-Grain <zwei.blattes@gmail.com>
2019-12-19 14:44:11 +0900

    不具合修正

--- fibonacci/FibonacciSequence.cpp
+++ fibonacci/FibonacciSequence.cpp
@@ -1,20 +1,20 @@
 #include "FibonacciSequence.h"
 #include <stdio.h>
 
-
 void  FibonacciSequence::GenerateFibonacciSequence(int64_t nth) {
   int64_t tmp[3] = {0, 1, 0};
   if (0 > nth) {
     printf("入力値(%lld)が不正です。\n", nth);
     return;
   }
+  if (0 == nth) {
+    _Print(nth, tmp[nth]);
+    return;
+  }
   for (int index = 0; index < 2; index++) {
     _Print(index, tmp[index]);
   }
-  if (2 >= nth) {
-    _Print(nth, tmp[nth]);
-  }
-  for (int index = 2; index < nth; index++) {
+  for (int index = 2; index <= nth; index++) {
     tmp[2] = tmp[0] + tmp[1];
     _Print(index, tmp[2]);
     tmp[0] = tmp[1];
--- fibonacci/main.cpp
+++ fibonacci/main.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <iostream>
 #include "FibonacciSequence.h"
 using namespace std;
